45d83fb6d7271ebfb507895eb3b0a37f11295a6a
mehran1421 <m.kamrani1421@gmail.com>
2021-05-10 21:50:10 +0430

    create table for django-dbBackup

--- dbBackup/models.py
+++ dbBackup/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100)
+    status = models.BooleanField(default=True)
+    position = models.IntegerField()
+
+    class Meta:
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    thumbnail=models.ImageField(upload_to='images')
+    category = models.ManyToManyField(Category, null=True)
+
+    def __str__(self):
+        return self.title
